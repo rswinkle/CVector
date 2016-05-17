bd89c295b95905cdf3a0894d41355f9d2f0afdf3
Robert Winkler <rob121618@gmail.com>
2016-05-17 14:18:02 -0700

    Hadn't run Doxygen with the latest

--- docs/search/defines_0.js
+++ docs/search/defines_0.js
@@ -1,4 +1,8 @@
 var searchData=
 [
+  ['cvec_5fd_5fallocator',['CVEC_D_ALLOCATOR',['../vector__d_8c.html#ac8fb90dc7cfcf6456fc114b469454711',1,'vector_d.c']]],
+  ['cvec_5fi_5fallocator',['CVEC_I_ALLOCATOR',['../vector__i_8c.html#a1c3b4d9cc9c546a4cc52ad391f4d8e6e',1,'vector_i.c']]],
+  ['cvec_5fstr_5fallocator',['CVEC_STR_ALLOCATOR',['../vector__str_8c.html#ad52d33ab8f1a1f6293838122272fef45',1,'vector_str.c']]],
+  ['cvec_5fvoid_5fallocator',['CVEC_VOID_ALLOCATOR',['../vector__void_8c.html#a216578f1df3db043455ff44621713ba7',1,'vector_void.c']]],
   ['cvector_5fimplementation',['CVECTOR_IMPLEMENTATION',['../main2_8cpp.html#a698a659125cddbafcfae778d1c5b3a91',1,'main2.cpp']]]
 ];
--- docs/search/defines_6.js
+++ docs/search/defines_6.js
@@ -1,10 +1,6 @@
 var searchData=
 [
-  ['vec_5fd_5fallocator',['VEC_D_ALLOCATOR',['../vector__d_8c.html#a024a194564bd2419ca1ac75e3cdec3e8',1,'vector_d.c']]],
-  ['vec_5fi_5fallocator',['VEC_I_ALLOCATOR',['../vector__i_8c.html#a6d9a8fd1aaa4a8df09d79da016a5c5aa',1,'vector_i.c']]],
-  ['vec_5fstr_5fallocator',['VEC_STR_ALLOCATOR',['../vector__str_8c.html#ac6b46abc3594994ede1811c5268fa72f',1,'vector_str.c']]],
   ['vec_5ftype_5fallocator',['VEC_TYPE_ALLOCATOR',['../vector__template3_8c.html#a9462161a6c0545da12081025af558853',1,'VEC_TYPE_ALLOCATOR():&#160;vector_template3.c'],['../vector__template4_8c.html#a9462161a6c0545da12081025af558853',1,'VEC_TYPE_ALLOCATOR():&#160;vector_template4.c']]],
-  ['vec_5fvoid_5fallocator',['VEC_VOID_ALLOCATOR',['../vector__void_8c.html#a6f86fe931c02dfbb3d901d2f5e5ec969',1,'vector_void.c']]],
   ['vector_5ff_5fstruct_5fimplementation',['VECTOR_f_struct_IMPLEMENTATION',['../main_8cpp.html#a1ba49dcccb3d4ba2912855afe5261bb9',1,'VECTOR_f_struct_IMPLEMENTATION():&#160;main.cpp'],['../vector__tests_8c.html#a1ba49dcccb3d4ba2912855afe5261bb9',1,'VECTOR_f_struct_IMPLEMENTATION():&#160;vector_tests.c']]],
   ['vector_5fshort_5fimplementation',['VECTOR_short_IMPLEMENTATION',['../main_8cpp.html#adae57a657e4d64cab28772cae36d5136',1,'VECTOR_short_IMPLEMENTATION():&#160;main.cpp'],['../vector__tests_8c.html#adae57a657e4d64cab28772cae36d5136',1,'VECTOR_short_IMPLEMENTATION():&#160;vector_tests.c']]],
   ['version',['VERSION',['../config_8h.html#a1c6d5de492ac61ad29aec7aa9a436bbf',1,'config.h']]]
