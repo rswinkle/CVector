2fee1aff98d6bcb5ef092a5281b4aab6c1c4b107
Robert Winkler <rob121618@gmail.com>
2016-06-09 11:43:40 -0700

    Forgot to remove stdio.h from make_cvector.py

--- cvector.h
+++ cvector.h
@@ -6,7 +6,6 @@
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include <assert.h>
 
 #ifdef __cplusplus
--- make_cvector.py
+++ make_cvector.py
@@ -8,7 +8,6 @@ cvector_str = """
 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 #include <assert.h>
 """
 
